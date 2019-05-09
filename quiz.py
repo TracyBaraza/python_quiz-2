0537f075115c4b634328c590f7bcca3c7136f443
TracyBaraza <barazatracy16@gmal.com>
2019-05-09 19:08:04 +0300

    my data structures

---
+++ quiz.py
@@ -0,0 +1,54 @@
+def list(numbers):
+	b = []
+	for x in numbers:
+		print(x%3)
+ 
+
+
+
+
+def sorted(a,b,c):
+	a = []
+	b = []
+	c = []
+	d = a + b + c
+	d.sort()
+	return d
+
+
+def divisible_by_three(n):
+	numbers = (1,n+1)
+	for a in numbers:
+		if a/3 == 0:
+			return numbers
+
+def flatten(x):
+	flat = []
+	for sublist in x:
+		for a in sublist:
+			flat.append(a)
+	return flat	
+
+
+def	smallest(b):
+    list = []
+    min(list)	
+
+def letters():
+ y = []
+ y.remove([0],[2],[3],[5],[7])
+ return x
+
+def squares(n):
+    a = dict()
+    for x in range(149,159):
+        if x**2==0:
+            a[x] = x**2
+       
+    return a                   
+
+
+		
+
+
+
